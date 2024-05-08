f87eee90b6b30cd5d8a5e5b6fa22ed6deac070e0
sina kazemi <dev.sinakazemi@gmail.com>
2024-05-08 17:33:34 +0330

    feat: add footer in landing app

---
+++ apps/landing/app/_components/layout/Footer.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import Link from "next/link"
+import { Button } from "@ui/components"
+import Logo from "~/app/_components/layout/Logo"
+import { GithubIcon, LinkedinIcon, SendIcon } from "lucide-react"
+
+const socialMedias = [
+  {
+    id: 1,
+    icon: <GithubIcon size={16} />,
+    url: "https://github.com/m-sina-k/CryptoHub",
+  },
+  {
+    id: 2,
+    icon: <SendIcon size={16} />,
+    url: "https://t.me/cphub_price_bot",
+  },
+  {
+    id: 3,
+    icon: <LinkedinIcon size={16} />,
+    url: "https://www.linkedin.com/in/m-sina-k/",
+  },
+]
+
+function Footer() {
+  return (
+    <footer className="border-1 border-twc_border mb-5 mt-16 flex items-center justify-between rounded-lg px-12 py-6 backdrop-blur-lg">
+      <section>
+        <Logo />
+      </section>
+      <section className="flex items-center gap-3">
+        {socialMedias.map((socialMedia) => (
+          <Button
+            key={socialMedia.id}
+            variant="ghost"
+            className="border-1 flex size-8 p-1"
+          >
+            <Link href={socialMedia.url}>{socialMedia.icon}</Link>
+          </Button>
+        ))}
+      </section>
+    </footer>
+  )
+}
+
+export default Footer
--- apps/landing/app/page.tsx
+++ apps/landing/app/page.tsx
@@ -1,6 +1,7 @@
 import Features from "~/app/_components/Features"
 import GetStarted from "~/app/_components/GetStarted"
 import HowToBegin from "~/app/_components/HowToBegin"
+import Footer from "~/app/_components/layout/Footer"
 import Header from "~/app/_components/layout/header/Header"
 import WhatDoWeDo from "~/app/_components/WhatDoWeDo"
 import Stats from "~/app/Stats"
@@ -15,6 +16,7 @@ export default function Home() {
         <HowToBegin />
         <Stats />
         <WhatDoWeDo />
+        <Footer />
       </div>
     </main>
   )
